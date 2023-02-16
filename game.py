05bd0898d24c2dc15865bcf7d5a68e66239dec7c
Gichogomary <marygichogo53@gmail.com>
2023-02-16 09:28:56 +0300

    added hellothere message

---
+++ game.py
@@ -0,0 +1,15 @@
+#this program demonstrates a guessing 
+#game
+#get user input
+#generate a random number
+#using a while loop
+#check if user input is equal to generated number
+username = input("what's your name")
+print("Hello there"+username+"!")
+
+
+#generate a random nmber outside
+number = randint
+counter = 0
+while counter < 5:
+    user_number = eval(input("enter a number:"))
